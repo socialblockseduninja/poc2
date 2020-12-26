d44b8e5b55d8aef6d19e299a8aece104ea75ea58
socialblockseduninja <76597188+socialblockseduninja@users.noreply.github.com>
2020-12-26 21:43:07 +0530

    Update user.py

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -17,12 +17,12 @@ class UserRepository:
         """ Update a user's age """
         user = self.get(last_name, first_name)
         user.age = age
-        out= json.dumps({'status': 'OK', 'topicDesc':'test'})
+        out= json.dumps( {"age": 25, "first_name": "John", "last_name": "Doe"})
         return out;
 
     @staticmethod
     def create(last_name, first_name, age):
         """ Create a new user """
          
-        out= json.dumps({'status': 'OK', 'topicDesc':'test'})
+        out= json.dumps({"age": 25, "first_name": "John", "last_name": "Doe"})
         return out;
