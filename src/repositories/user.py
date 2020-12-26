4abab8cff1e9fe430758390f2c18f774a90dadad
socialblockseduninja <76597188+socialblockseduninja@users.noreply.github.com>
2020-12-27 03:50:44 +0530

    fvg
    sdf

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -1,6 +1,7 @@
 """ Defines the User repository """
 import json
 from models import User
+from flask import Response
 
 
 class UserRepository:
@@ -17,8 +18,10 @@ class UserRepository:
         """ Update a user's age """
         user = self.get(last_name, first_name)
         user.age = age
-        out= json.dumps( {"user": {"age": 30, "first_name": "John", "last_name": "Doe"}})
-        return out;
+        
+        out= json.dumps( {"status_code":200,"user": {"age": 30, "first_name": "John", "last_name": "Doe"}})
+        
+        return Response(out, status=200, mimetype='application/json');
 
     @staticmethod
     def create(last_name, first_name, age):
